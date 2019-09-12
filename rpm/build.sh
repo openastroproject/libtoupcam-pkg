9d963cb769c378a8ef7f606294fb0db857e0bad9
James Fidell <james@openastroproject.org>
2019-09-12 20:36:28 +0100

    Updates for latest toupcam SDK release

--- rpm/build.sh
+++ rpm/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -f libtoupcam-1.33.13725.tar.gz
-ln ../libtoupcam-1.33.13725.tar.gz .
+rm -f libtoupcam-1.39.15529.tar.gz
+ln ../libtoupcam-1.39.15529.tar.gz .
 rel=`cut -d' ' -f3 < /etc/redhat-release`
 fedpkg --release f$rel local
