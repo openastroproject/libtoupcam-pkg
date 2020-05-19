446ea5161177bf821094946718cf8925656451a0
James Fidell <james@openastroproject.org>
2020-05-19 20:25:32 +0100

    Remove version dependency

--- rpm/build.sh
+++ rpm/build.sh
@@ -3,7 +3,7 @@
 cp ../patches/*.patch .
 rm -f makefile.patch
 
-rm -f libtoupcam-1.39.15529.tar.gz
-ln ../libtoupcam-1.39.15529.tar.gz .
+rm -f libtoupcam-*.tar.gz
+ln ../libtoupcam-*.tar.gz .
 rel=`cut -d' ' -f3 < /etc/redhat-release`
 fedpkg --release f$rel local
